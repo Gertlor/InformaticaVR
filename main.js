0d282b31e8ea05f2c8da55e50795e5134566fe8f
Gerrit Louis <gerritlouis@gmail.com>
2019-06-18 15:33:30 +0200

    add uml dialog

--- main.js
+++ main.js
@@ -7,24 +7,72 @@ let teacher = document.getElementById("js--teacher");
 let dialogBox = document.getElementById("js--dialogBox");
 let dialogText = document.getElementById("js--dialogText");
 let nextDialog = document.getElementById("js--nextButton");
+let beginButton = document.getElementById("js--beginButton");
 
 let timeoutTimeforTeleportation = 1500;
+let textIndex;
 
-showTeleportationPad = () => {
-  teleportationPad.setAttribute("visible", "true");
-  teleportationPad.setAttribute("position", "0 0.01 -2");
-  teleportationPad.onmouseenter = () => {
-    teleportAudio.play();
-    setTimeout(teleport = () => {
-        cameraRig.setAttribute("position", "0 1.9 -2");
-    }, timeoutTimeforTeleportation)
-  }
+startUmlDrawing = () => {
+console.log("drawing")
+
 };
 
 startUmlDialog = () => {
-  console.log("starting UML Dialog.")
+  let umlTextArray = [
+    "We gaan een klassendiagram tekenen van de classe Main.java met een methode Hello World.",
+    "Om deze opdracht te realiseren moet je de componenten pakken die je nodig hebt en deze op de goede plek zetten op het bord."
+  ];
+  textIndex = 1;
 
+  cameraRig.setAttribute("rotation", "0 30 0");
+  teacher.setAttribute("position", "-0.5 0 -6");
+  teacher.setAttribute("rotation", "0 -100 0");
 
+  dialogBox.setAttribute("position", " -1.5 2.7 -5.");
+  dialogBox.setAttribute("rotation", "0 30 0");
+  dialogBox.setAttribute("visible", "true");
+
+  dialogText.setAttribute("position", " -1.46 2.7 -4.95");
+  dialogText.setAttribute("rotation", "0 30 0");
+  dialogText.setAttribute("text", "value: " + umlTextArray[0]);
+  dialogText.setAttribute("text", "width: 1.9");
+  dialogText.setAttribute("visible", "true");
+
+  nextDialog.setAttribute("position", "-1 2.35 -4.8");
+  nextDialog.setAttribute("rotation", "0 30 0");
+  nextDialog.setAttribute("visible", "true");
+
+  nextDialog.onmouseenter = () => {
+    if(textIndex >= 0 && textIndex < umlTextArray.length){
+      dialogText.setAttribute("text", "value: " + umlTextArray[textIndex]);
+      textIndex++;
+      if(textIndex === umlTextArray.length){
+        nextDialog.setAttribute("visible", "false");
+        beginButton.setAttribute("visible", "true");
+        beginButton.setAttribute("rotation","0 30 0");
+        beginButton.setAttribute("position", "-1.4 2.35 -4.8");
+        beginButton.onmouseenter = () => {
+          setTimeout(draw = () => {
+            startUmlDrawing();
+          }, 1000)
+        };
+      }
+    }
+  }
+};
+
+showTeleportationPad = () => {
+  teleportationPad.setAttribute("visible", "true");
+  teleportationPad.setAttribute("position", "0 0.01 -2.5");
+  teleportationPad.onmouseenter = () => {
+    teleportAudio.play();
+    setTimeout(teleport = () => {
+        dialogBox.setAttribute("visible", "false");
+        dialogText.setAttribute("visible", "false");
+        cameraRig.setAttribute("position", "0 1.9 -2.5");
+        startUmlDialog();
+    }, timeoutTimeforTeleportation)
+  }
 };
 
 startInitialDialog = () => {
@@ -33,11 +81,11 @@ startInitialDialog = () => {
     "Als eerst gaan we wat UML tekeken, ik zal op je wachten in de ruimte achter mij.\n\n" +
     "PS. Je kan het gele stip voor je gebruiken om daar naar toe te gaan."
   ];
-  let textIndex = 0;
+  textIndex = 0;
   nextDialog.onmouseenter = () => {
     if(textIndex >= 0 && textIndex < initialTextArray.length){
       dialogText.setAttribute("text", "value: " + initialTextArray[textIndex]);
-      textIndex++
+      textIndex++;
       if(textIndex === initialTextArray.length){
         nextDialog.setAttribute("visible", "false");
         showTeleportationPad()
@@ -45,7 +93,6 @@ startInitialDialog = () => {
     }
   }
 };
-
 
 startInitialDialog();
 
