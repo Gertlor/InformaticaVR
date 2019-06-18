c6f6385fdadba8a6a25e0d21aabd1578aa57aa70
Gerrit Louis <gerritlouis@gmail.com>
2019-06-18 14:41:12 +0200

    add initial dialog

--- main.js
+++ main.js
@@ -7,14 +7,46 @@ let teacher = document.getElementById("js--teacher");
 let dialogBox = document.getElementById("js--dialogBox");
 let dialogText = document.getElementById("js--dialogText");
 let nextDialog = document.getElementById("js--nextButton");
-let backDialog = document.getElementById("js--backButton");
 
-let timeoutTimeforTeleportation = 500;
+let timeoutTimeforTeleportation = 1500;
 
-startInitialDialog();
+showTeleportationPad = () => {
+  teleportationPad.setAttribute("visible", "true");
+  teleportationPad.setAttribute("position", "0 0.01 -2");
+  teleportationPad.onmouseenter = () => {
+    teleportAudio.play();
+    setTimeout(teleport = () => {
+        cameraRig.setAttribute("position", "0 1.9 -2");
+    }, timeoutTimeforTeleportation)
+  }
+};
+
+startUmlDialog = () => {
+  console.log("starting UML Dialog.")
+
+
+};
 
 startInitialDialog = () => {
-  
+  let initialTextArray = [
+    "Ik ben Gerrit en vandaag ga ik je rondleiden om een indruk te krijgen van de opleiding Informatica.",
+    "Als eerst gaan we wat UML tekeken, ik zal op je wachten in de ruimte achter mij.\n\n" +
+    "PS. Je kan het gele stip voor je gebruiken om daar naar toe te gaan."
+  ];
+  let textIndex = 0;
+  nextDialog.onmouseenter = () => {
+    if(textIndex >= 0 && textIndex < initialTextArray.length){
+      dialogText.setAttribute("text", "value: " + initialTextArray[textIndex]);
+      textIndex++
+      if(textIndex === initialTextArray.length){
+        nextDialog.setAttribute("visible", "false");
+        showTeleportationPad()
+      }
+    }
+  }
 };
+
+
+startInitialDialog();
 
 
